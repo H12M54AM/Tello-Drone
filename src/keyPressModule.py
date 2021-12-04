925df70600d36cb96dbc82d7ec94b8f4000bdd1d
Edward Naidoo <securedbread@protonmail.com>
2021-12-03 19:06:27 -0800

    Changes to the Keyboard files
    Small fix that had to me made as well as a new file using the previous file as a module

--- src/keyPressModule.py
+++ src/keyPressModule.py
@@ -11,22 +11,20 @@ def getKey(key):
     ans = False
     for e in pygame.event.get(): pass
     input = pygame.key.get_pressed()
-    # Formats to k_blank where blank is what key you want to be pressed
-    myKey = getattr(pygame, 'k{}'.format(key))
+    # Formats to K_blank where blank is what key you want to be pressed
+    myKey = getattr(pygame, 'K_{}'.format(key))
     if input[myKey]:
         ans = True
     pygame.display.update()
     return ans
 
+# Allows which key on the keyboard gets pressed
 def main():
     if getKey("LEFT"):
         print("Left Key Entered")
     if getKey("RIGHT"):
         print("Right Key Entered")
-    if getKey("UP"):
-        print("Up Key Entered")
-    if getKey("Down"):
-        print("Down Key Entered")
+
 
 if __name__ == "__main__":
     init()
---
+++ src/keyboardControl.py
@@ -0,0 +1,6 @@
+import keyPressModule as kpm
+
+kpm.init()
+
+while True:
+    print(kpm.getKey("s"))
